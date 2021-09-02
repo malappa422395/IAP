b94690ef2cfb709b34d40e982fefe530531ab4e9
dcharlis <dcharlis@pkglobal.com>
2021-09-02 13:23:42 +0530

    Adding tailwind as cdn and card component

--- iap-app/src/App.js
+++ iap-app/src/App.js
@@ -1,10 +1,14 @@
 import Card from './components/Card';
 import './App.css';
-
+import data from './mockdata/homepage.json';
 function App() {
   return (
-    <div className="w-full h-screen flex justify-center items-center bg-gray-200">
-      <Card />
+    <div className="grid gap-4 grid-cols-3 bg-gray-200 p-8">
+      {
+        data && data.assets && data.assets.map((asset,index)=> {
+            return  <Card key={index} asset={asset} />
+        })
+      }
     </div>
   );
 }
--- iap-app/src/components/Card.js
+++ iap-app/src/components/Card.js
@@ -1,23 +1,29 @@
-import kerasImage from './../images/keras.png';
+import kerasImage from './../images/keras_logo.png';
 
-function Card() {
+function Card(props) {
+  console.log(props)
   return (
     <div className="bg-white rounded-lg p-4 shadow-lg max-w-sm">
       <div className="flex items-center">
-        <img className="w-14" src={kerasImage} alt="brand"></img>
-        <div className="ml-6 text-red-500 text-lg font-semibold">Keras</div>
+        <img className="w-14" src={props.asset.image} alt="brand"></img>
+        <div className="ml-6 text-red-500 text-lg font-semibold">{props.asset.name}</div>
       </div>
       <hr className="border-gray-400 border-1 mt-4" />
       <div className="text-gray-400 text-xs mt-2 font-medium">Updated 5hrs ago</div>
-      <p className="mt-3 text-sm font-semibold mb-3 text-gray-600">Keras is a open-source software library for dataflow and differentiable ...</p>
+      <p className="mt-3 text-sm font-semibold mb-3 text-gray-600 overflow-ellipsis overflow-hidden h-16">{props.asset.description}</p>
       <hr className="border-gray-400 border-1 mb-4" />
      
       <div className="grid gap-2 grid-cols-3">
-        <span className="bg-gray-200 px-5 py-1 rounded-md text-xs text-gray-500 font-semibold text-center">Container</span>
-        <span className="bg-gray-200 px-5 py-1 rounded-md text-xs text-gray-500 font-semibold text-center">x86</span>
-        <span className="bg-gray-200 px-5 py-1 rounded-md text-xs text-gray-500 font-semibold text-center">Azure VM</span>
-        <span className="bg-gray-200 px-5 py-1 rounded-md text-xs text-gray-500 font-semibold text-center">Linux</span>
-        <span className="bg-gray-200 px-5 py-1 rounded-md text-xs text-gray-500 font-semibold col-span-2 text-center">Programming Language</span>
+        {props.asset.techlogies.map((tech,index)=>{
+            if(props.asset.techlogies.length === 5 && index === 4){
+              return(
+                <span key={index} className="bg-gray-200 px-5 py-1 rounded-md text-xs text-gray-500 font-semibold text-center col-span-2">{tech.name}</span>
+              )
+            }
+            return(
+              <span key={index} className="bg-gray-200 px-5 py-1 rounded-md text-xs text-gray-500 font-semibold text-center">{tech.name}</span>
+            )
+        })}
       </div>
     </div>
   );
