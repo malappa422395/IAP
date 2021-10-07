5bbb26a5c845633cf83e919467665646c034ef46
malappa422395 <65086479+malappa422395@users.noreply.github.com>
2021-10-07 19:17:57 +0530

    As discussed with prasad i did changes and commit
    As discussed with prasad i did changes and commit

--- iap-app/src/components/TensorFlow/TensorFlowHeader.js
+++ iap-app/src/components/TensorFlow/TensorFlowHeader.js
@@ -9,7 +9,7 @@ function TensorFlowHeader() {
                         <div><img src={'images/tf_logo.png'} alt="" height="100px" weight="100px"/></div>
                     </div>
                     <div style={{flex:"8"}}>
-                        <div style={{fontWeight:"700", color:"rgb(221, 53, 53)",paddingTop:".6rem"}}>TensorFlow 2.0</div>
+                        <div style={{fontWeight:"700", color:"rgb(221, 53, 53)",paddingTop:".6rem"}}>PK-Score</div>
                         <div style={{color:'#8e8e8e',fontSize:'11px',fontWeight:"500"}}>Updated 5hr ago</div>
                     </div>
                 </div>
@@ -21,8 +21,9 @@ function TensorFlowHeader() {
                     <span class="bg-gray-200 py-1 rounded-md text-xs text-gray-500 font-semibold text-center">Docker</span>
                     <span class="bg-gray-200 py-1 rounded-md text-xs text-gray-500 font-semibold text-center">AMI</span></div>
             </div>
-            <div style={{margin:"1rem 0rem",flex:"4",fontSize:"14px",borderLeft:"1px solid #cdcdcd",padding:"0rem 1rem"}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type 
-</div>
+            <div style={{margin:"1rem 0rem",flex:"4",fontSize:"14px",borderLeft:"1px solid #cdcdcd",padding:"0rem 1rem"}}>
+                Access the maturity of current DevOps tools and provides a Radar graph of current score with the industry standard.
+            </div>
         </div>
     )
 }
