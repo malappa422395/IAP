4b336c5ba5242c393c613e13cd1dd16d2eb8f38a
malappa422395 <65086479+malappa422395@users.noreply.github.com>
2021-10-13 12:44:44 +0530

    changes commits
    changes commits

--- iap-app/src/components/Filter.js
+++ iap-app/src/components/Filter.js
@@ -5,16 +5,16 @@ function Filter(props) {
 
     return (
         <div className="overflow-y-auto max-h-screen">
-            <div key="Filters" className="ml-10 mr-5 mb-20">
+            <div key="Filters" className="ml-10 mr-5 mb-20 cursor-pointer">
                 {filterData.Filters.map((data, index) => {
                     return (
                         <div key={data.Type} className="mt-3">
                             <div className="text-white mb-2">{data.Type}</div>
                             {data.list.map(data => {
                                 return (
-                                    <div key={data} >
-                                        <label className="inline-flex items-center" onChange={props.getCheckedFilter.bind(this,data)}>
-                                            <input type="checkbox" className="form-checkbox" />
+                                    <div key={data}>
+                                        <label className="inline-flex items-center cursor-pointer" onChange={props.getCheckedFilter.bind(this,data)}>
+                                            <input type="checkbox" className="form-checkbox cursor-pointer" />
                                             <span className="ml-2 text-white">{data}</span>
                                         </label>
                                     </div>
